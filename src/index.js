090a42946b53bb2dca5f6401b0393910103c0936
chibitofu <erinmoony@gmail.com>
2020-01-08 15:05:57 -0800

    Working game.

--- src/index.js
+++ src/index.js
@@ -32,6 +32,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -49,7 +52,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
